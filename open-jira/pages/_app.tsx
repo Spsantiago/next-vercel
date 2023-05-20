b6dc3e977c756d336fb6a29e1afa2302153ba245
Santiago Paredes <Spsantiago0410@gmail.com>
2023-05-20 11:53:47 -0500

    creacion de nuevo proyecto

---
+++ open-jira/pages/_app.tsx
@@ -0,0 +1,17 @@
+import '@/styles/globals.css'
+import { darkTheme } from '@/themes'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+
+import type { AppProps } from 'next/app'
+
+
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+<ThemeProvider theme={darkTheme}>
+    <CssBaseline/>
+
+  <Component {...pageProps} />
+</ThemeProvider>  
+  )
+}
