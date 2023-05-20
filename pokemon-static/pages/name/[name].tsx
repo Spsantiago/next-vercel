7375e2af3afb37d01b7bd4cc0941e9bda2c0309a
Santiago Paredes <Spsantiago0410@gmail.com>
2023-05-20 11:55:22 -0500

    creacion de nuevo proyecto

--- pokemon-static/pages/name/[name].tsx
+++ pokemon-static/pages/name/[name].tsx
@@ -120,17 +120,26 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     const pokemonNames: string[] = data.results.map((pokemon) => pokemon.name);
     return {
         paths: pokemonNames.map((name) => ({ params: { name } })),
-        fallback: false,
+        fallback: 'blocking',
     };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
     const { name } = params as { name: string };
-   
+    
+    const pokemon = await getPokemonInfo(name)
+    if (!pokemon){
+        return{
+           redirect:{
+               destination:'/',
+               permanent:false
+           }
+        }
+   }
     return {
         props: {
-            pokemon: await getPokemonInfo(name)
-        },
+            pokemon
+        },revalidate: 86400,
     };
 };
 export default PokemonByNamePage;
--- pokemon-static/utils/getpokemoninfo.ts
+++ pokemon-static/utils/getpokemoninfo.ts
@@ -1,13 +1,16 @@
-import { pokeAPI } from "@/api";
-import { Pokemon } from "@/interfaces";
+import { pokeAPI } from '@/api';
+import { Pokemon } from '@/interfaces';
 
-export const getPokemonInfo= async(nameOrId:string)=>{
-    
-    const { data } = await pokeAPI.get<Pokemon>(`/pokemon/${nameOrId}`);
+export const getPokemonInfo = async (nameOrId: string) => {
+    try {
+        const { data } = await pokeAPI.get<Pokemon>(`/pokemon/${nameOrId}`);
 
-    return {
-        id : data.id,
-        name: data.name,
-        sprites: data.sprites
+        return {
+            id: data.id,
+            name: data.name,
+            sprites: data.sprites,
+        };
+    } catch (error) {
+        return null;
     }
-}
+};
